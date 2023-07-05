6c57629dd738d6d2ea8c475da0a6efc73a5ef507
Mustafa Alsihati <51824138+MustafaAlsihati@users.noreply.github.com>
2023-07-05 15:43:16 +0300

    Small Fixes

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,12 @@ import onSendDocuSignEmail from './docusign';
 import { DocuSignConfig, Signature, Signer } from './types';
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.post('/send', async (req, res) => {
+const router = express.Router();
+
+router.post('/send', async (req, res) => {
   try {
     const privateKey = fs.readFileSync(`${__dirname}/keys/private.key`);
     let configs: DocuSignConfig = {
@@ -16,14 +20,31 @@ app.post('/send', async (req, res) => {
       clientId: process.env.DOCUSIGN_CLIENT_ID!,
       userId: process.env.DOCUSIGN_USER_ID!,
       oAuthBasePath: process.env.OAUTHBASEPATH!,
+      redirectUri: process.env.REDIRECT_URI!,
       privateKey,
       // * Request Body:
-      redirectUri: req.body.redirectUri as string,
       htmls: req.body.htmls as string[],
       emailSubject: req.body.emailSubject as string,
       signers: req.body.signers as Signer[],
       signatures: req.body.signatures as Signature[],
     };
+
+    if (!configs.htmls || !configs.htmls.length) {
+      throw new Error('No HTMLs provided');
+    }
+
+    if (!configs.signers || !configs.signers.length) {
+      throw new Error('No Signers provided');
+    }
+
+    if (!configs.signatures || !configs.signatures.length) {
+      throw new Error('No Signatures provided');
+    }
+
+    if (!(configs.emailSubject ?? '').length) {
+      throw new Error('No Email Subject provided');
+    }
+
     // * Send DocuSign Email:
     const result = await onSendDocuSignEmail(configs);
     // ? Redirect to Consent URL if User Token is not valid:
@@ -34,9 +55,13 @@ app.post('/send', async (req, res) => {
     // = Send Result Back to Client:
     res.send(result);
   } catch (error) {
+    console.error('[ERROR]', error);
     res.status(500).send({ message: error.message });
   }
 });
+
+// * Routes:
+app.use('/api', router);
 
 // ? Listen on port 5000:
 app.listen(process.env.PORT ?? 5000, () => {
